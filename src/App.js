a1fe85eebcd76218da26d11698b1fe1dc9afac0e
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-12 22:10:57 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import TodoApp from './component/TodoApp';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
 const App = () => {
   return (
+    <Switch>
+    <Route path='/' exact>
+      <Redirect path = '/home' />
+    </Route>
+    <Route path='/home' exact>
     <div className="App">
       <span className='title'>Todo list</span>
       <TodoApp />
-    </div>
+      </div>
+    </Route>
+    </Switch>
   );
 }
 
