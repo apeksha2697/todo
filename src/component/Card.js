cf3bb897bcfb11810596f8fbee92ba4ce1d5e952
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-14 17:23:41 +0530

    added navbar to card

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,14 +1,18 @@
+import NavigationBar from './Navigationbar';
 import TodoApp from './TodoApp';
 import '../App.css';
 import './Card.css';
 
 const Card = () => {
 	return(
+		<div>
+		<NavigationBar />
 		<div className='card'>
 			<div className="app">
 				<span className='title'>Todo list</span>
 				<TodoApp />
 			</div>
+		</div>
 		</div>
 	);
 };
