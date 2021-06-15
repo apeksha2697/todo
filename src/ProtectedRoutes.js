1d546681ca1b62784a944b2c162e334c22cbef46
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-15 15:12:15 +0530

    protected routes

---
+++ src/ProtectedRoutes.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+class ProtectedRoute extends Component {
+  render() {
+    const Component = this.props.component;
+    return this.props.isLoggedin ? (
+      <div>
+        <Component isLoggedin={this.props.isLoggedin} setisLoggedin={this.props.setisLoggedin}/>
+      </div>
+    ) : (
+      <Redirect to={{ pathname: "/signup" }} />
+    );
+  }
+}
+
+export default ProtectedRoute;
